84785c915237aaf928786b1b0c85f573e0cfe341
handongxu <handong_xu@163.com>
2026-01-30 09:32:29 +0800

    update

--- src/services/__init__.py
+++
@@ -1,7 +0,0 @@
-# encoding : utf-8 -*-                            
-# @author  : 冬瓜                              
-# @mail    : dylan_han@126.com    
-# @Time    : 2026/1/4 11:08
-from .llm_service import LLMService
-
-__all__ = ['LLMService']
--- start.sh
+++
@@ -1,2 +0,0 @@
-export WEBSOCKET_AUTH_KEY="my-debtbot-secret-2026"
-uvicorn api:app --host 0.0.0.0 --port 8000
